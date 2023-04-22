d98044c09ccdf706cd06ce88b5d7a770a8927d50
fpetrakov <jakephin2015@gmail.com>
2023-04-22 14:27:18 +0300

    declarative

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,15 @@ import {
 	flip,
 	curry,
 	__,
+	multiply,
+	inc,
+	dec,
+	negate,
+	defaultTo,
+	cond,
+	equals,
+	always,
+	T,
 } from 'ramda'
 
 const tasks = [
@@ -23,7 +32,7 @@ const tasks = [
 
 const incomplete = filter(whereEq({ complete: false }))
 const twoIncomplete = compose(take(2), incomplete)
-console.log(twoIncomplete(tasks))
+twoIncomplete(tasks)
 
 // Simple combinators
 
@@ -38,8 +47,8 @@ find(either(isEven, isFour))([1, 2, 4, 5, 6]) // 2
 
 // Pipe
 const sum = (a: number, b: number) => a + b
-const square = (a: number) => a * a
-const squaredSum = pipe(sum, square)
+const mySquare = (a: number) => a * a
+const squaredSum = pipe(sum, mySquare)
 squaredSum(2, 2) // 16
 
 // Partial application
@@ -53,3 +62,21 @@ flipped(v => v * 2, [1, 2, 3]) // order of arguments changed, so it's easier to 
 const threeArgs = curry((a, b, c) => a + b + c)
 const middleArgumentLater = threeArgs(1, __, 3)
 middleArgumentLater(10) // 14
+
+// Arithmetics
+const square = (x: number) => multiply(x, x)
+const operate = pipe(negate, dec, inc, square)
+
+// Logic
+const settings: { lineWidth?: number } = {}
+// checks if the second argument null or undefined
+const lineWidth = defaultTo(80, settings.lineWidth)
+
+const water = temperature =>
+	cond([
+		[equals(0), always('water freezes at 0 C')],
+		[equals(100), always('water boils at 100 C')],
+		[T, temp => `nothing special happens at ${temp} C`],
+	])(temperature)
+
+water(100) // water boils at 100 C
